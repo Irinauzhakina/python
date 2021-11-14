ecaf2481e89a4bf7a94985baa8ea4884363e7fa5
Irinauzhakina <86876292+Irinauzhakina@users.noreply.github.com>
2021-11-14 10:48:52 +0300

    Update lesson 4 task 5.py
    done

--- lesson 4 task 5.py
+++ lesson 4 task 5.py
@@ -1,1 +1,9 @@
+from functools import reduce
 
+my_list = [i for i in range (100, 1002) if i % 2 ==0]
+#def fun_1 (a, b) 
+#    return  my_list (i)*my_list(i+1)
+
+a = reduce (lambda a,b  : a*b, my_list)
+
+print (a)
